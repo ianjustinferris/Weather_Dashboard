dff1524cf51293a473296522e6185df7ed476ef7
ianjustinferris <ianjustinferris@gmail.com>
2022-04-14 00:02:15 -0400

    CSS

---
+++ Assets/script.js
@@ -0,0 +1,8 @@
+  mapboxgl.accessToken =
+        "pk.eyJ1IjoiaWFuanVzdGluZmVycmlzIiwiYSI6ImNsMXUzdWFrdjI5YzEzY3BjcTN2bHdxcXkifQ.nHDp49alvjpiTFbRUmWL0Q";
+      var map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/ianjustinferris/cl1ya9otw001515qmp86s6l21",
+        center: [-84.3880,33.7490],
+        zoom: 9
+      });
